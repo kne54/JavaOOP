b73ab91f6f1d41d4cb307d349f1db1e630d499f6
kne54 <kne@ngs.ru>
2019-07-10 16:20:58 +0700

    Range task change

---
+++ Range/src/ru/ngs/kne/java/oop/Range.java
@@ -0,0 +1,98 @@
+package ru.ngs.kne.java.oop;
+
+public class Range {
+    private double from;
+    private double to;
+
+    public Range(double from, double to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    public double getFrom() {
+        return from;
+    }
+
+    public double getTo() {
+        return to;
+    }
+
+    public void setFrom(double from) {
+        this.from = from;
+    }
+
+    public void setTo(double to) {
+        this.to = to;
+    }
+
+    public void print(){
+        System.out.println(this.from + " " + this.to);
+    }
+
+    public void checkOrder() {
+        if (to < from) {
+            double temp = from;
+            from = to;
+            to = temp;
+        }
+    }
+
+    public double getLength() {
+        return to - from;
+    }
+
+    public boolean isInside(double x) {
+        return (x >= from && x <= to);
+    }
+
+    public Range getIntersection(Range range) {
+        double from = range.getFrom();
+        double to = range.getTo();
+        double maxFrom;
+        double minTo;
+
+        if (this.from >= from) {
+            maxFrom = this.from;
+        } else {
+            maxFrom = from;
+        }
+
+        if (this.to <= to) {
+            minTo = this.to;
+        } else {
+            minTo = to;
+        }
+
+        if (maxFrom <= minTo) {
+            return new Range(maxFrom, minTo);
+        }
+
+        return null;
+    }
+
+    public Range[] combine(Range range) {
+        if (this.getIntersection(range) == null) {
+            return new Range[]{this, range};
+        }
+
+        double from = range.getFrom();
+        double to = range.getTo();
+        double minFrom;
+        double maxTo;
+
+        if (this.from <= from) {
+            minFrom = this.from;
+        } else {
+            minFrom = from;
+        }
+
+        if (this.to >= to) {
+            maxTo = this.to;
+        } else {
+            maxTo = to;
+        }
+
+        return new Range[]{new Range(minFrom, maxTo)};
+    }
+}
+
