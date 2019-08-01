534d808c414b00c00935e0ac8a6022816b6c4a45
kne54 <kne@ngs.ru>
2019-08-01 11:07:23 +0700

    + Circle

---
+++ Shapes/src/ru/ngs/kne/java/oop/Circle.java
@@ -0,0 +1,29 @@
+package ru.ngs.kne.java.oop;
+
+public class Circle extends Shapes {
+    double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double getWidth() {
+        return 2 * radius;
+    }
+
+    @Override
+    public double getHeight() {
+        return 2 * radius;
+    }
+
+    @Override
+    public double getArea() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2 * Math.PI * radius;
+    }
+}
