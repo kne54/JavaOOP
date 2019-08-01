2db7e00b3cc37d78d1ada23fb1f371e4e4cee4da
kne54 <kne@ngs.ru>
2019-08-01 11:00:06 +0700

    Replace package Shapes

---
+++ Shapes/src/ru/ngs/kne/java/oop/Shapes.java
@@ -0,0 +1,11 @@
+package ru.ngs.kne.java.oop;
+
+public abstract class Shapes {
+    public abstract double getWidth();
+
+    public abstract double getHeight();
+
+    public abstract double getArea();
+
+    public abstract double getPerimeter();
+}
